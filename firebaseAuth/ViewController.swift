40797733dcb8d726b3ed3d6d086a00a6ba6606ba
Nurjamal <n.mirbaizaeva@gmail.com>
2023-09-18 21:16:34 +0600

    Initial Commit

---
+++ firebaseAuth/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  firebaseAuth
+//
+//  Created by Nurjamal Mirbaizaeva on 18/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
